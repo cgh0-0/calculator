25f357c6d31b43989f701ab17b078b83ebae3fc0
cgh0-0 <marketcarrot280@gmail.com>
2025-08-06 00:28:15 +0900

    Add files via upload
    처음 시도

---
+++ calculator/계산기/계산기/계산기1.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+
+template<typename T>
+T add(T a, T b)
+{
+	return a + b;
+}
+
+template<typename T>
+T minus(T a, T b)
+{
+	return a - b;
+}
+
+template<typename T>
+T multiple(T a, T b)
+{
+	return a * b;
+}
+
+template<typename T>
+T division(T a, T b)
+{
+	if (b == 0) {
+		std::cout << "The divisor cannot be zero" << std::endl;
+		return "Pls turn off the program and then turn it back on.";
+	}
+	return a / b;
+}
+
+	
+	
+
+template<typename T>
+int main()
+{
+	T a;
+	T b;
+
+	std::cout << "Please enter the first number to calculate" << std::endl;
+	std::cin >> a;
+	std::cout << "Please enter the second number to calculate" << std::endl;
+	std::cin >> b;
+	char operation;
+
+	std::cout << "Please enter the desired operation among +, -, *, /." << std::endl;
+	std::cin >> operation;
+	switch (operation)
+	{
+	case'+':add(a,b)
+	case'-':minus(a, b);
+	case'*':multiple(a, b);
+	case '/':division(a,b)
+
+	}
+	std::cout<<a<<operation<<b<<"="<<
+}
