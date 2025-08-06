9411a5bf5adbb3a30783c69363fe90c4076fd315
cgh0-0 <marketcarrot280@gmail.com>
2025-08-06 16:39:22 +0900

    정수형 아니면 실수형
    뭔가 매끄럽게 되지 않는다. 문법들을 다시 보고 이걸 뭔가 더 간단하고 이해하기 쉽게 보여 줄 수 있는 방법을 찾아야겠다.

--- calculator/계산기/계산기/계산기1.cpp
+++ calculator/계산기/계산기/계산기1.cpp
@@ -1,5 +1,15 @@
 #include<iostream>
-
+#include<string>
+class integer
+{public:
+	int a;
+	int b;
+};
+class realnum
+{
+	double a;
+	double b;
+};
 template<typename T>
 T add(T a, T b)
 {
@@ -28,10 +38,65 @@ T division(T a, T b)
 	return a / b;
 }
 
-
-
-int main()
+void calculatorint()
 {
+	int a;
+	int b;
+	std::cout << "Please enter the first number to calculate" << std::endl;
+	std::cin >> a;
+	std::cout << "Please enter the second number to calculate" << std::endl;
+	std::cin >> b;
+	char operation;
+
+	std::cout << "Please enter the desired operation among +, -, *, /." << std::endl;
+	std::cin >> operation;
+	int result;
+	switch (operation)
+	{
+	case'+':result = add(a, b);
+		break;
+	case'-':result = minus(a, b);
+		break;
+	case'*':result = multiple(a, b);
+		break;
+	case '/':result = division(a, b);
+		break;
+
+	}
+	std::cout << a << operation << b << "=" << result << std::endl;
+	std::cout << "Would you like to perform an operation on the result? y/n" << std::endl;
+	char yn;
+	std::cin >> yn;
+	do
+	{
+		a = result;
+		std::cout << "Please enter the second number to calculate" << std::endl;
+		std::cin >> b;
+		char operation;
+
+		std::cout << "Please enter the desired operation among +, -, *, /." << std::endl;
+		std::cin >> operation;
+		int result;
+		switch (operation)
+		{
+		case'+':result = add(a, b);
+			break;
+		case'-':result = minus(a, b);
+			break;
+		case'*':result = multiple(a, b);
+			break;
+		case '/':result = division(a, b);
+			break;
+
+		}
+		std::cout << a << operation << b << "=" << result << std::endl;
+		std::cout << "Would you like to perform an operation on the result? y/n" << std::endl;
+		std::cin >> yn;
+	} while (yn == 'Y' || yn == 'y');
+
+	std::cout << a << operation << b << "=" << result;
+}
+void realnumcalculator() {
 	double a;
 	double b;
 
@@ -60,7 +125,7 @@ int main()
 	std::cout << "Would you like to perform an operation on the result? y/n" << std::endl;
 	char yn;
 	std::cin >> yn;
-	do 
+	do
 	{
 		a = result;
 		std::cout << "Please enter the second number to calculate" << std::endl;
@@ -80,14 +145,36 @@ int main()
 			break;
 		case '/':result = division(a, b);
 			break;
-			
+
 		}
 		std::cout << a << operation << b << "=" << result << std::endl;
 		std::cout << "Would you like to perform an operation on the result? y/n" << std::endl;
 		std::cin >> yn;
 	} while (yn == 'Y' || yn == 'y');
-		
+
 	std::cout << a << operation << b << "=" << result;
+}
+
+int main()
+{
+
+
+	std::cout << "Do you want to calculate with integers or with real numbers ?" << std::endl;
+	std::string intorreal;
+	std::cin >> intorreal;
+	
+	if (intorreal == "interger"||intorreal=="int")
+	{
+		calculatorint();
+	}
+	else if (intorreal == "realnum"||intorreal=="real")
+	{	
+		realnumcalculator();
+	}
+
+	
+
+	
 			
 	
 	return 0;
